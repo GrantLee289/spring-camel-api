c42a2982dcb1573084c12ad76da79f30d8ce9ccc
GrantLee289 <grantlee289@gmail.com>
2020-05-07 11:37:19 +0100

    removed unused imports

--- src/main/java/com/grantlee289/springcamel/Application.java
+++ src/main/java/com/grantlee289/springcamel/Application.java
@@ -1,11 +1,7 @@
 package com.grantlee289.springcamel;
 
-import org.apache.camel.component.servlet.CamelHttpTransportServlet;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
